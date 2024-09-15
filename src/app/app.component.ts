cea28dc9e6440e959f5719335e79960fb4cabdbc
mahmoud hegazy <mhf2662003@gmail.com>
2024-09-15 20:23:01 +0300

    new create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/navbar/nav-bar/nav-bar.component';
+import { AllProductsComponent } from './components/products/all-products/all-products.component';
+import { ProductsDetailsComponent } from './components/products/products-details/products-details.component';
+import { CartComponent } from "./components/carts/cart/cart.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, AllProductsComponent, ProductsDetailsComponent, CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
