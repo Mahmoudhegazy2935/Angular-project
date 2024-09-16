54d6c0133ed686d4042553a3b97098833b0b8d17
Eman Refai <eman.ahmed01092738@gmail.com>
2024-09-16 06:11:45 +0200

    new edit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CartComponent } from './components/carts/cart/cart.component';
+import { ProductsDetailsComponent } from './components/products/products-details/products-details.component';
+import { AllProductsComponent } from './components/products/all-products/all-products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'products', component: AllProductsComponent },
+{ path: 'details', component: ProductsDetailsComponent },
+{ path: 'cart', component: CartComponent},
+{ path: '**', redirectTo: 'products', pathMatch: 'full' } ,];
--- src/app/components/products/all-products/all-products.component.ts
+++ src/app/components/products/all-products/all-products.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AllProductsService } from '../../../services/all-products.service';
+import { Product } from '../../../product/product';
+import { CartsService } from '../../../services/carts.service';
 
 @Component({
   selector: 'app-all-products',
@@ -7,6 +10,16 @@ import { Component } from '@angular/core';
   templateUrl: './all-products.component.html',
   styleUrl: './all-products.component.css'
 })
-export class AllProductsComponent {
+export class AllProductsComponent  {
+products:Product[]=[];
+item: any;
+data: any;
+constructor(private productservice:CartsService){}
 
-}
+
+ 
+  ngOnInit():void{
+  
+  this.productservice.getAllproducts().subscribe (data =>{this.products=data 
+     console.log(this.products)
+  });}}
